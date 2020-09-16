49777701a9bb759797cd2bb4b70bfb34e2c47b7c
Allen Nicholson <ranicholson@git.com>
2020-09-16 20:36:58 +0000

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main- printf
+ * description: prints the text in quotes
+ * Return: The value is 0
+ */
+
+int main(void)
+
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+		return (0);
+}
