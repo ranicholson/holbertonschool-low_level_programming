8f48fac44268bcecfe800f7d01fe27d6a6818e68
Allen Nicholson <ranicholson@git.com>
2020-09-16 20:10:00 +0000

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,9 @@
 include < stdio.h >
-int puts('"'Programming is like building a multilingual puzzle)
+
+int main(void)
+
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
