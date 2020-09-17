c3d3d2314aa123d73334d61f5e7e91c4cbaa4144
Allen Nicholson <ranicholson@git.com>
2020-09-17 04:17:15 +0000

    Task 9

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main- fwrite
+ * description:write to binary stream output
+ * Return: should be 1
+ */
+
+int main(void)
+{
+	fprint(STDERR,
+	       "and that piece of art is useful\" \- Dora Korpar, 2014\-10\-19/n);
+
+	       return (1)
+}
