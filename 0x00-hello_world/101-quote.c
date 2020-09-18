8524307fc5069791c6b14399fadc6bfa64f6d170
Allen Nicholson <ranicholson@git.com>
2020-09-18 18:36:22 +0000

    Task 9

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,7 @@
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2014-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2014-10-15\n", 59);
 
 	return (1);
 }
