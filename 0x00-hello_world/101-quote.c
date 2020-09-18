63973bfed1f19a7da6afb690a23e01f0823424b4
Allen Nicholson <ranicholson@git.com>
2020-09-18 18:34:04 +0000

    Task 9

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main- fwrite
@@ -8,8 +9,7 @@
 
 int main(void)
 {
-	fprint(STDERR,
-	       "and that piece of art is useful\" \- Dora Korpar, 2014\-10\-19/n);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2014-10-19\n", 59);
 
-	       return (1)
+	return (1);
 }
