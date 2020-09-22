8ac76e83e0564e33f3ef40c5fcd614c1a900e435
Allen Nicholson <ranicholson@git.com>
2020-09-22 14:27:09 +0000

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * _abs - absolute value function
+ *@r: number generated
+ * description: function to compute absolute value of a number
+ * Return: Hopefully 0!
+ */
+
+int _abs(int r)
+{
+	if (r >= 0)
+		return (r);
+	else
+		return (-r);
+}
