7d8fad9397ea566d5c3f36c5e1f24bd44959cd48
Allen Nicholson <ranicholson@git.com>
2020-09-22 17:51:18 +0000

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * add - adds integers
+ * @x: represents a number
+ * @y: represents a number
+ * Return: return should bbe the vaue of two integers added together
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
