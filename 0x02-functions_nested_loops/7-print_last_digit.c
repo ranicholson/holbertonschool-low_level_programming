0acd7bb982d929195d5828a310c01b7b353a0d2a
Allen Nicholson <ranicholson@git.com>
2020-09-22 14:41:02 +0000

    Task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - function to print last digit of a number
+ * @x: random number
+ * Return: Hopefully 0!
+ */
+
+int print_last_digit(int x)
+{
+	_putchar((x % 10));
+
+	return (0);
+}
