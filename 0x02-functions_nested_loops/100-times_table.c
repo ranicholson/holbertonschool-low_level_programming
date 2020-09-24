d227f2733cd00acd33eb299d69bfde1910fb79a3
Allen Nicholson <ranicholson@git.com>
2020-09-24 02:02:53 +0000

    100 times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,48 @@
+#include "holberton.h"
+
+/**
+ * times_table - function to print times table
+ */
+
+void print_times_table(int n)
+{
+	int x;
+	int y;
+
+	if (n > 15 || n < 0)
+		_putchar('\n');
+
+	else
+	{
+		for (x = 0; x <= n; x++)
+		{
+			_putchar('0');
+			for (y = 1; y <= n; y++)
+			{
+				int z = x * y;
+				_putchar(',');
+				_putchar(' ');
+
+				if (z >= 10 && z < 100)
+				{
+					_putchar(' ');
+					_putchar((z / 10) + '0');
+					_putchar((z % 10) + '0');
+				}
+				else if (z >= 100)
+				{
+					_putchar((z / 100) + '0');
+					_putchar(((z / 10) / 10) + '0');
+					_putchar((z % 10) + '0');
+				}
+				else
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(z + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
