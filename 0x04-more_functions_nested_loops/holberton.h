2bd71d3c73d334a9ae99bb721c7bd35e2dbfa11a
Allen Nicholson <ranicholson@git.com>
2020-09-24 22:00:26 +0000

    Header file

---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,22 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/**
+ * main- Header file
+ * description: Linking functions to other files
+ * Return: No return on a header.
+ */
+
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+
+#endif
