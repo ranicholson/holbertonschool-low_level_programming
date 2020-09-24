bc29f35d2a581b214a56aece3440d17708546a43
Allen Nicholson <ranicholson@git.com>
2020-09-24 13:08:26 +0000

    Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -32,17 +32,11 @@ void print_times_table(int n)
 					_putchar((z / 10) + '0');
 					_putchar((z % 10) + '0');
 				}
-				else if (z > 100)
+				else if (z >= 100)
 				{
 					_putchar((z / 100) + '0');
-					_putchar(((z / 10) / 10) + '0');
+					_putchar(((z / 10) % 10) + '0');
 					_putchar((z % 10) + '0');
-				}
-				else if (z == 100)
-				{
-					_putchar('1');
-					_putchar('0');
-					_putchar('0');
 				}
 				else
 				{
