32874e1a934a3e2efdfcbe05c696342d701c35b7
Allen Nicholson <ranicholson@git.com>
2020-09-25 02:49:58 +0000

    Task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * print_square - print out squares!
+ * @size: size of the square you want
+ *
+ */
+
+void print_square(int size)
+{
+	int x;
+	int y;
+
+	if (size > 0)
+	{
+		for (x = 1; x <= size; x++)
+		{
+			for (y = 1; y <= size; y++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
