b072216d766cad9110c251c30797895ac1790595
Allen Nicholson <ranicholson@git.com>
2020-09-25 03:34:56 +0000

    Task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,15 +13,16 @@ int main(void)
 	for (x = 1; x <= 100; x++)
 	{
 		if ((x % 3) == 0 && (x % 5) == 0)
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		else if ((x % 3) == 0)
-			printf("Fizz ");
+			printf("Fizz");
 		else if ((x % 5) == 0)
-			printf("Buzz ");
+			printf("Buzz");
 		else if (x == 100)
-			printf("%d", x);
+			continue;
 		else
-			printf("%d ", x);
+			printf("%d", x);
+		printf(" ");
 	}
 	printf("\n");
 
