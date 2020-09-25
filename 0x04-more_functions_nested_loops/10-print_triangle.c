f8f0596d0bad1308f6b9535f6a93e3177952f163
Allen Nicholson <ranicholson@git.com>
2020-09-25 03:29:49 +0000

    Task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * print_triangle - draw a nice triangle
+ *@size: How long the line should be
+ * 
+ */
+
+void print_triangle(int size)
+{
+	int x;
+	int y;
+
+	if (size > 0)
+	{
+		for (x = 1; x <= size; x++)
+		{
+			for (y = size - x; y > 0; y--)
+				_putchar(' ');
+
+			for (y = 0; y < x; y++)
+				_putchar('#');
+
+			if (x == size)
+				continue;
+
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
