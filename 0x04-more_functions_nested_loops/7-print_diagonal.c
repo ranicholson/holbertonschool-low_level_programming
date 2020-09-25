f08c71c834ebc56e060666f9fe1e82cedfc35405
Allen Nicholson <ranicholson@git.com>
2020-09-25 01:31:47 +0000

    Task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - draw a nice diagonal line
+ *@n: How long the line should be
+ * The x is the length of the line and y is the space
+ */
+
+void print_diagonal(int n)
+{
+	int x;
+	int y;
+
+	if (n > 0)
+	{
+		for (x = 0; x < n; x++)
+		{
+			for (y = 0; y < x; y++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+
+			if (x == n)
+				continue;
+
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
