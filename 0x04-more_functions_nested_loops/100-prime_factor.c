9c82ec08ad270ff27ace5eba158415404e2a08cf
Allen Nicholson <ranicholson@git.com>
2020-09-25 15:26:29 +0000

    Task 11

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -8,26 +8,24 @@
 
 int main(void)
 {
-	long int x = 1231952;
+	long int x = 612852475143;
 	long int y = 2;
 	long int maxp;
 
 	while (x != 0)
+	{
+		if (x % y != 0)
+			y++;
+		else
 		{
-			if (x % y != 0)
-				y++;
-			else
+			maxp = x;
+			x = x / y;
+			if (x == 1)
 			{
-				maxp = x;
-				x = x / y;
-				if (x == 1)
-				{
-					printf("%ld", maxp);
-					break;
-				}
+				printf("%ld\n", maxp);
+				break;
 			}
 		}
-	printf("\n");
-
+	}
 	return (0);
 }
