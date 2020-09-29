5d0a0026a595baa19898126b4d8524af6ef0c0cd
Allen Nicholson <ranicholson@git.com>
2020-09-29 15:05:14 +0000

    Task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * swap_int - swap the value of two integers
+ * @a: variable to swap
+ * @b: variable to swap
+ *
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
