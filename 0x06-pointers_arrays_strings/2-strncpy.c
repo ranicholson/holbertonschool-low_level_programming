ab957015a277ec1ebfeba2b71347b390c2337904
Allen Nicholson <ranicholson@git.com>
2020-10-01 14:17:58 -0500

    Task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * *_strncpy - copies a string
+ * @dest: destination for string to be copied to
+ * @src: source string to be copied
+ * @n: mavimum to be copied
+ * Return: should be dest after copy
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int x;
+
+	for (x = 0; x < n && src[x] != '\0'; x++)
+	{
+		dest[x] = src[x];
+	}
+
+	dest[x] = '\0';
+
+	return (dest);
+}
