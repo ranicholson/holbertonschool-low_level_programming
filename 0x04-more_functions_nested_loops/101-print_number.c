ea026ac50b9f5aed466298e1843b281a5161e2c7
Allen Nicholson <ranicholson@git.com>
2020-10-02 14:38:15 -0500

    Task 12

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * print_number - trying to print an integer
+ * @n: integer to print
+ *
+ */
+
+void print_number(int n)
+{
+	unsigned int x = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		x *= -1;
+	}
+
+	if ((x / 10) > 0)
+	{
+		print_number(x / 10);
+	}
+
+	_putchar((x % 10) + 48);
+}
