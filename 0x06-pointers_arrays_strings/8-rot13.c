d670fd7c05b28486b5b0d7d6265e81645db2532d
Allen Nicholson <ranicholson@git.com>
2020-10-02 10:25:30 -0500

    Task 8

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * rot13 - function to encode a string
+ * @str: string to be encoded
+ * Return: encoded string
+ */
+
+char *rot13(char *str)
+{
+
+	int x;
+	int y;
+	char z[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+	for (x = 0; str[x]; x++)
+	{
+		for ( y = 0; a[y]; y++)
+		{
+			if (str[x] == a[y])
+			{
+				str[x] = z[y];
+				break;
+			}
+		}
+	}
+
+	return (str);
+}
