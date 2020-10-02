dbd1e936ba40aa39d9c72ac9685b73dda9a7b6ea
Allen Nicholson <ranicholson@git.com>
2020-10-02 14:12:03 -0500

    Task 9

---
+++ 0x06-pointers_arrays_strings/100-print_numbers.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * print_number - print an integer
+ * @n:integer to be printed
+ *
+ *
+ */
+
+void print_number(int n)
+{
+	unsigned int x = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		x *= -1;
+	}
+
+	if ((x / 10) > 0)
+	{
+		print_number(x / 10);
+	}
+
+	_putchar((x % 10) + 48);
+}
