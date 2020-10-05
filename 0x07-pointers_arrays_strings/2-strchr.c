76cfc2dafcdddd421e49115b390eefe364e00fa0
Allen Nicholson <ranicholson@git.com>
2020-10-05 14:42:02 -0500

    Task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * _strchr - function to locate character in string
+ * @s: string to be searched
+ * @c: character to search for
+ * Return: pointer to first occurence of the character
+ */
+
+char *_strchr(char *s, char c)
+{
+	int x;
+
+	for (x = 0; s[x]; x++)
+	{
+		if (s[x] == c)
+		{
+			return (s + x);
+		}
+	}
+
+	return ('\0');
+}
