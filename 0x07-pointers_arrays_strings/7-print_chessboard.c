19d5df300c89fdd155f361308b762b157e5650c1
Allen Nicholson <ranicholson@git.com>
2020-10-05 20:03:38 -0500

    Task 6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * print_chessboard - function to print the chess board
+ * @a: array with chessboard info
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int x;
+	int y;
+
+	for (x = 0; x < 8; x++)
+	{
+		for (y = 0; y < 8; y++)
+		{
+			_putchar(a[x][y]);
+		}
+		_putchar('\n');
+	}
+}
