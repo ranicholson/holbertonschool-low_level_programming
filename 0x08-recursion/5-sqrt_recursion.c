15a26df767118a43d284ba68c1cbe71d9c84c4de
Allen Nicholson <ranicholson@git.com>
2020-10-07 15:56:13 -0500

    Task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,27 @@
 #include "holberton.h"
 
 /**
+ * sqrt_assist - help fin square of a number
+ * @n: number to find root of
+ * @x: square root
+ * Return: square root
+ */
+
+int sqrt_assist(int n, int x)
+{
+	if (x * x == n)
+		return (x);
+
+	if (x > (n / 2))
+		return (-1);
+
+	if (n < 0)
+		return (-1);
+
+	return sqrt_assist( n, (x + 1));
+}
+
+/**
  * _sqrt_recursion - find natural square root of a number
  * @n: number to find natural square of
  * Return: Natural square or -1 if there isn't one
@@ -8,13 +29,9 @@
 
 int _sqrt_recursion(int n)
 {
-	int x = 1;
+	int x = 0;
 
-	if ((x * x) == n)
-		return (x);
+	n = sqrt_assist(n, x);
 
-	x++;
-	_sqrt_recursion(n);
-
-	return (-1);
+	return (n);
 }
