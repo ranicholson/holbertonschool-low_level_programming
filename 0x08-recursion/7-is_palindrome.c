473b0927dd24e881af4b8b5d4d2f77cd0c21b2f2
Allen Nicholson <ranicholson@git.com>
2020-10-07 18:54:53 -0500

    Task 7

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -1,6 +1,45 @@
 #include "holberton.h"
 
 /**
+ * pal_assist - help to determine if a string is a palindrome
+ * @s: string to check
+ * @x: length of string
+ * @y: section of string
+ * Return: 1 if palindrome and 0 if not
+ */
+
+int pal_assist( char *s, int x)
+{
+	if (*s != s[x])
+		return (0);
+
+	if (*s == '\n')
+		return (1);
+
+	return (pal_assist(s + 1, x - 2));
+}
+
+/**
+ * _strlen_recursion - find length of string
+ * @s: string to find length of
+ * Return: the length of the string
+ */
+
+int _strlen_recursion(char *s)
+{
+	int x = 0;
+
+	if (*s)
+	{
+		x++;
+		x += _strlen_recursion(s + 1);
+	}
+
+	return (x);
+}
+
+
+/**
  * is_palindrome - function to determine if string is palindrome
  * @s: string to determine palindrimity of
  * Return: 1 if palindrome and 0 if not
@@ -9,29 +48,9 @@
 int is_palindrome(char *s)
 {
 	int x = _strlen_recursion(s);
-	int y = 0;
 
-	if (*s == (*s + (x - 1)))
-		return (pal_assist(s, x, y));
+	if (*s == *(s + x))
+		return (pal_assist(s, x));
 
 	return (0);
-}
-
-/**
- * pal_assist - help to determine if a string is a palindrome
- * @s: string to check
- * @x: length of string
- * @y: section of string
- * Return: 1 if palindrome and 0 if not
- */
-
-int pal_assist( char *s, int x, int y)
-{
-	if (s[y] != s[x - 1])
-		return (0);
-
-	if (s[y + 1] == '\n')
-		return (1);
-
-	pal_assist(y + 1, x - 1);
 }
