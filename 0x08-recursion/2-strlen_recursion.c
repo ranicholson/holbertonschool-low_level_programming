f0929b79d526380d5b09845cdbe7ea3817ffeb13
Allen Nicholson <ranicholson@git.com>
2020-10-07 12:46:09 -0500

    Task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _strlen_recursion - find length of string
+ * @s: string to find length of
+ * Return: the length of the string
+ */
+
+int _strlen_recursion(char *s)
+{
+	int x = 0;
+
+	if (*s)
+	{
+		x++;
+		x += _strlen_recursion(s + 1);
+	}
+
+	return (x);
+}
