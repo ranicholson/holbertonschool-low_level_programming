30c043defcd03997a77c78dc4b6531d8c8af4a06
Allen Nicholson <ranicholson@git.com>
2020-10-12 15:59:02 -0500

    Task 5

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+/**
+ * main - trying to figure out coins for change
+ * @argc - length of string
+ * @argv - amount to give change for
+ * Return: 0 for success and 1 for ultimate failure
+ */
+
+int main(int argc, char *argv[])
+{
+	int x;
+	int y;
+
+	if (argc == 2)
+	{
+		y = atoi(argv[1]);
+		while (y != 0)
+		{
+			if (y >= 25)
+			{
+				y -= 25;
+				x++;
+			}
+			else if (y % 10 == 0)
+			{
+				y -= 10;
+				x++;
+			}
+			else if (y % 5 == 0)
+			{
+				y -= 5;
+				x++;
+			}
+			else if (y % 2 == 0)
+			{
+				y -= 2;
+				x++;
+			}
+			else  if (y % 1 == 0)
+			{
+				y -= 1;
+				x++;
+			}
+		}
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", x);
+	return (0);
+}
