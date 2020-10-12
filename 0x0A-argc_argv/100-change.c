d1197bedfb62f24cf53ade914e14aaa671bb350e
Allen Nicholson <ranicholson@git.com>
2020-10-12 18:47:09 -0500

    Task 5

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -15,36 +15,38 @@ int main(int argc, char *argv[])
 	if (argc == 2)
 	{
 		y = atoi(argv[1]);
-		while (y > 0)
+		while (y != 0)
 		{
-			if (y % 25 == 0)
+			if (y < 0)
+				break;
+			else if (y >= 25)
 			{
 				y -= 25;
 				x++;
 			}
-			else if (y % 10 == 0)
+			else if (y >= 10)
 			{
 				y -= 10;
 				x++;
 			}
-			else if (y % 5 == 0)
+			else if (y >= 5)
 			{
 				y -= 5;
 				x++;
 			}
-			else if (y % 2 == 0)
+			else if (y >= 2)
 			{
 				y -= 2;
 				x++;
 			}
-			else  if (y % 1 == 0)
+			else  if (y >= 1)
 			{
 				y -= 1;
 				x++;
 			}
 		}
 	}
-	else
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
