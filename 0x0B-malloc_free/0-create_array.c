1260564cb1dcda82e4cf8094a8fe5144686fa121
Allen Nicholson <ranicholson@git.com>
2020-10-13 16:12:13 -0500

    Task 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * *create_array - function to create array of characters
+ * @size: size of array
+ * @c: contents of string
+ * Return: Null if the size is 0 or if it fails, and a pointer to the array upon
+ * success
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	unsigned int x;
+
+	array = malloc(size * sizeof(char));
+
+	if (size == 0 || array == NULL)
+		return (NULL);
+
+	for (x = 0; x < size; x++)
+		array[x] = c;
+
+	return (array);
+}
