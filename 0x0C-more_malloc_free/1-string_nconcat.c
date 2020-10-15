8497de5f6546af6d6e71090556b0db04d4e2d014
Allen Nicholson <ranicholson@git.com>
2020-10-15 15:35:03 -0500

    Task 1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,59 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * _strlen - determine the length of a string
+ * @s: string to determine the length of
+ * Return: should be the length
+ *
+ */
+
+int _strlen(char *s)
+{
+	int l = 0;
+
+	while (s[l] != '\0')
+		l++;
+
+	return (l);
+}
+/**
+ * *string_nconcat - concatenate two strings
+ * @s1: first string to concatenate
+ * @s2: second string to concatenate
+ * @n: length of s2 to concatenate
+ * Return: pointer to the location of completed concatenation or NULL for fail
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int x, y, z = n, a ,b;
+	char *conc;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s1 = "";
+
+	x = _strlen(s1);
+	y = _strlen(s2);
+
+	if (n > y)
+		z = y;
+
+	conc = malloc((sizeof(char) * (x + z)) + 1);
+
+	if (conc == NULL)
+		return (NULL);
+
+	for (a = 0; a < x; a++)
+		conc[a] = s1[a];
+
+	for (b = 0; b < z; b++)
+		conc[b + a] = s2[b];
+
+	conc[b + a + 1] = '\0';
+
+	return (conc);
+}
