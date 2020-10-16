41f08e96b1e2cea676cee5cab3b53b40aaa14d22
Allen Nicholson <ranicholson@git.com>
2020-10-16 13:23:44 -0500

    Task 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -21,10 +21,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (ptr == NULL)
 	{
 		ptr = malloc(new_size);
-
-		if (ptr == NULL)
-			return (NULL);
-
 		return (ptr);
 	}
 
