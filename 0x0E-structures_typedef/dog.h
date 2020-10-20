e9490ca2bc6a00901a6219694ea25639cff64b2d
Allen Nicholson <ranicholson@git.com>
2020-10-19 19:32:10 -0500

    Task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,5 @@
 #ifndef DOG_H
 #define DOG_H
-
 /**
  * struct dog - Dog struct to hold data on dog
  * @name: Dog name
@@ -9,18 +8,13 @@
  *
  * Description: Structure to hold name, age, and owner of a dog
  */
-
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
 int _putchar(char c);
-
 #endif
