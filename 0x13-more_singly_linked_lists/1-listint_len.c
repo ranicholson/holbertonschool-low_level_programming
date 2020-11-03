efc96798b3dd99e50b2ddb5e3aaaaf7e0a019a3b
ranicholson <ranicholson@git.com>
2020-11-03 08:51:26 -0600

    Task 1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include  "lists.h"
+
+/**
+ * listint_len - functio to return number of elements in a list
+ * @h: list to count the number of elements of
+ * Return: number of elements
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t ne;
+
+	while (h)
+	{
+		ne++;
+		h = h->next;
+	}
+
+	return (ne);
+}
