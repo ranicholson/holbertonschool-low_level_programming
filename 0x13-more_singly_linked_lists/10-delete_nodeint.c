a92bf394c4654524e984cff45002a7ee7499e067
ranicholson <ranicholson@git.com>
2020-11-04 16:43:23 -0600

    Task 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,6 +1,32 @@
 #include "lists.h"
 
 /**
+ * get_nodeint_at_index - function that returns the nth node of a linked list
+ * @head: list to get node from
+ * @index: index of the node to return
+ * Return: node referenced by the index or NULL if it does not exist
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int y = 0;
+
+	while (head != NULL)
+	{
+		if (y == index)
+			break;
+
+		head = head->next;
+		y++;
+	}
+
+	if (y < index)
+		return (NULL);
+
+	return (head);
+}
+
+/**
  * delete_nodeint_at_index - function to delete node referenced by index
  * @head: list to remove node from
  * @index: node that should be removed
