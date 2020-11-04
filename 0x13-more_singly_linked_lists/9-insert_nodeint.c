a3b620123b2bb6c19f0f3b2bcd51b126aefe8377
ranicholson <ranicholson@git.com>
2020-11-04 16:41:53 -0600

    Task 9 update

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,5 +1,33 @@
 #include "lists.h"
 
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - function that returns the nth node of a linked list
+ * @head: list to get node from
+ * @index: index of the node to return
+ * Return: node referenced by the index or NULL if it does not exist
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int y = 0;
+
+	while (head != NULL)
+	{
+		if (y == index)
+			break;
+
+		head = head->next;
+		y++;
+	}
+
+	if (y < index)
+		return (NULL);
+
+	return (head);
+}
+
 /**
  * insert_nodeint_at_index - function to insert new node at given position
  * @head: list to insert node into
