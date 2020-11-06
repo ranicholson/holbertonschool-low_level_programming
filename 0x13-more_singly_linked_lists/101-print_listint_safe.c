595043d5c7c78fc3af0db4c5a63c5a0e6ed3775a
ranicholson <ranicholson@git.com>
2020-11-06 13:57:57 -0600

    Task 12

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - function to print a linked list
+ * @head: head to print out
+ * Return: number of nodes in list
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	unsigned int count = 0;
+	const listint_t *j = head;
+
+	if (head == NULL)
+		return (count);
+
+	while (j)
+	{
+		if (j == head->next->next && count > 3)
+			return (count);
+
+		printf("[%p] %d\n", (void *)j, j->n);
+		j = j->next;
+		count++;
+	}
+
+	return (count);
+}
