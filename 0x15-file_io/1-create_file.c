a50bf94b73a362255abe6afe50a160e46ba92653
ranicholson <ranicholson@git.com>
2020-11-09 15:50:03 -0600

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	op = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	op = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
 	if (op == -1)
 		return (0);
