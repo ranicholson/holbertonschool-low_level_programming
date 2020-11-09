cbca4d04d8c8d8c304ac72949b3b18f754f27c36
ranicholson <ranicholson@git.com>
2020-11-09 15:40:40 -0600

    Task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+
+/**
+ * create_file - function to create a file
+ * @filename: name of file to create
+ * @text_content: string to write to file
+ * Return: 1 for success and -1 for failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int x, op, wr;
+
+	if (filename == NULL)
+		return (-1);
+
+	op = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+
+	if (op == -1)
+		return (0);
+
+	for (x = 0; text_content[x]; x++)
+		;
+
+	if (x == 0)
+		wr = write(op, '\0', 1);
+
+	else
+		wr = write(op, text_content, x);
+
+	if (wr == -1)
+		return (-1);
+
+	close(op);
+
+	return (1);
+}
