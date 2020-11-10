0f52210552f63c02d694a635f381f284086cdca4
ranicholson <ranicholson@git.com>
2020-11-10 10:46:50 -0600

    Task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -32,7 +32,8 @@ int main(int argc, char *argv[])
 		werr(argv[2]);
 	}
 	buffy = buff_friend(argv[1]);
-	while (re > 0)
+	re = read(oporg, buffy, 2014);
+	while (re != 0)
 	{
 		re = read(oporg, buffy, 1024);
 		if (re == -1)
