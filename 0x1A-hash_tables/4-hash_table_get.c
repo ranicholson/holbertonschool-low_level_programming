c582c1669cd86343065ba1717a6e4038a1d43026
ranicholson <ranicho2@gmail.com>
2021-01-15 16:58:58 -0600

    Task 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,6 +12,9 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int idx;
 	hash_node_t *tmp = NULL;
 
+	if (ht == NULL || key == NULL)
+		return (NULL);
+
 	idx = key_index((unsigned char *)key, ht->size);
 	if (ht->array[idx] == NULL)
 		return (NULL);
