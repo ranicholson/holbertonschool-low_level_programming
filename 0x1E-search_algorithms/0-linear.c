cd29157c7882b334985e6a8df4e2ac51f4fbed74
ranicholson <ranicho2@gmail.com>
2021-04-20 18:33:03 +0000

    Task 0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -17,7 +17,7 @@ int linear_search(int *array, size_t size, int value)
 
 	while (x < y)
 	{
-		printf("Value checked array [%d] = [%d]\n", x, array[x]);
+		printf("Value checked array[%d] = [%d]\n", x, array[x]);
 		if (array[x] == value)
 			return (x);
 		x += 1;
